47affc45fa5d2f89904694ebb5992f9d26ea9454
asboff <egor622722@gmail.com>
2022-09-05 18:54:29 +0300

    task 1 commit

---
+++ database/migrations/2022_08_30_114509_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('description', 1000);
+            $table->integer('category_id');
+            $table->string('image', 255);
+            $table->decimal('price');
+            $table->boolean('active');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
