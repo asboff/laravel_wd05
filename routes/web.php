b4381186f39786ff0d97e00fcf098338050d65ab
asboff <egor622722@gmail.com>
2022-09-28 21:51:35 +0300

    28 September lesson

--- routes/web.php
+++ routes/web.php
@@ -28,10 +28,10 @@ Route::get('/store', StoreController::class);
 
 Route::get('/cart', [CartController::class, 'getCart']);
 Route::get('/add_to_cart', [CartController::class, 'addToCart']);
-Route::get('/test', function (){
-//    $product = \App\Models\Product::inRandomOrder()->first();
-    $category = Category::inRandomOrder()->first();
-    dd($category->products);
+Route::post('/test', function (\Illuminate\Http\Request $request){
+    $data = $request->all();
+
+    return response()->json($data)->setStatusCode(401);
 });
 
 Auth::routes();
